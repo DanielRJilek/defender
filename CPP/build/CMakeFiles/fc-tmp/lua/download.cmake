bae0454416e3d0d4a3c12d2cad9b1eeb8ca6dacd
Daniel Jilek <jilek.d@northeastern.edu>
2026-08-13 16:53:00 -0500

    Delete CPP/build directory

--- CPP/build/CMakeFiles/fc-tmp/lua/download.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing download step for lua")
-
-block(SCOPE_FOR VARIABLES)
-
-include("C:/Users/djroc/NEU/2026Summer/CS5850/110-finalproject-DanielRJilek/CPP/build/CMakeFiles/fc-tmp/lua/lua-gitclone.cmake")
-
-endblock()
--- CPP/build/game/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/djroc/NEU/2026Summer/CS5850/110-finalproject-DanielRJilek/CPP/game
-# Build directory: C:/Users/djroc/NEU/2026Summer/CS5850/110-finalproject-DanielRJilek/CPP/build/game
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
